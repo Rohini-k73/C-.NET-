52227e21808a9ca486cb6b15c57042f7a5073c5f
Rohini Kulkarni <kulkarnirohini879@gmail.comgit config --global user.emailgit config --global user.email kulkarnirohini879@gmail.com>
2025-03-20 23:46:44 +0530

    Add new

---
+++ Practice1/MyProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace Practice1
+{
+   internal class MyProgram
+    {
+        public static void Main() {
+            Student s1 = new Student();
+            s1.acceptData();
+            s1.display();
+
+        }
+    }
+}
